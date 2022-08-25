fd111b7f57f93f50514a12613312e363da009296
pachara171 <pachara1045@gmail.com>
2022-08-25 07:33:47 +0700

    3.Flex Box

--- components/Forecast.js
+++ components/Forecast.js
@@ -1,15 +1,22 @@
 import React from "react"
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 export default function Forecast(props) {
     return (
-    <View >
-        <Text>{props.main}</Text>
-        <Text>{props.description}</Text>
+    <View>
+        <Text style={styles.style_text}>{props.main}</Text>
+        <Text style={styles.style_text}>{props.description}</Text>
         <View>
-            <Text>{props.temp}</Text>
-            <Text>°C</Text>
+            <Text style={styles.style_text}>{props.temp}°C</Text>
         </View>
     </View>
     );
    }
+   const styles = StyleSheet.create({
+    style_text: {
+        color: "white",
+        textAlign: 'center',
+        padding: 20,
+        fontSize: 18,
+    },
+});
--- components/Weather.js
+++ components/Weather.js
@@ -1,28 +1,35 @@
 import React, { useState } from "react";
 import Forecast from "./Forecast";
 import { ImageBackground, StyleSheet, Text, View } from "react-native";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
 export default function Weather(props) {
     const [forecastInfo, setForecastInfo] = useState({
-    main: '-',
-    description: '-',
+    main: 'main',
+    description: 'description',
     temp: 0
     })
 
     return (
         <View>
-            <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
-                <Text>Zip Code</Text>
-                <Text>{props.zipCode}</Text>
+            <ImageBackground source={require('../bg.jpg')} style={styles.backdrop} >
+                <Text style={styles.style_text}>Zip Code  is  {props.zipCode}</Text>
                 <Forecast {...forecastInfo} />
             </ImageBackground>
         </View>
  );
 }
 const styles = StyleSheet.create({
-    backdrop: {
-    alignItems: 'center',
-    width: '100%',
-    height: '100%'
+    backdrop: {    
+        paddingTop: 50.00,
+        flexDirection: 'column',
+        alignItems: 'center',
+        width: '100%',
+        height: '100%'
     },
+    style_text: {
+        color: "white",
+        paddingTop: 30,
+        fontSize: 18
+    }
 });
